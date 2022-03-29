c61f5584b4cea207347843dca3a1c8d9fc4d0862
bgm064 <jani.aulavuo@myy.haaga-helia.fi>
2022-03-29 22:18:09 +0300

    Initial commit

---
+++ Programming 2/src/rajapinnat/Country.java
@@ -0,0 +1,25 @@
+package rajapinnat;
+
+public class Country implements Comparable<Country> {
+
+    private final String name;
+    private final int population;
+
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + ", population: " + this.population;
+    }
+}
